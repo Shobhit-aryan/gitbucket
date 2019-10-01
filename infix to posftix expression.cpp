a48492ccebd9fb44aac618972529157ff5a858c6
Shobhit Aryan Lal <55244666+Shobhit-aryan@users.noreply.github.com>
2019-10-01 19:47:56 +0530

    Create infix to posftix expression.cpp

---
+++ infix to posftix expression.cpp
@@ -0,0 +1,72 @@
+#include<bits/stdc++.h>
+using namespace std;
+int prec(char c)
+{
+    if(c=='^')
+        return 3;
+    else if(c=='*'||c=='/')
+         return 2;
+    else if(c=='+'||c=='-')
+        return 1;
+    else return -1;
+
+}
+void intopost(string s)
+{
+    stack<char> st;
+    st.push('N');
+    int l=s.length();
+    string ns;
+    for(int i=0;i<l;i++)
+    {
+        if((s[i]>='a'&&s[i]<='z')||(s[i]>='A'&&s[i]<='Z'))
+            ns=ns+s[i];
+        else if(s[i]=='(')
+                    st.push('(');
+        else if(s[i]==')')
+        {
+            while(st.top()!='N'&&st.top()!='(')
+            {
+                char c=st.top();
+                st.pop();
+                ns=ns+c;
+            }
+            if(st.top()=='(')
+            {
+                char c=st.top();
+                st.pop();
+            }
+        }
+        else
+        {
+            while(st.top()!='N'&&prec(s[i])<=prec(st.top()))
+            {
+                char c=st.top();
+                st.pop();
+                ns+=c;
+            }
+            st.push(s[i]);
+        }
+    }
+    while(st.top()!='N')
+    {
+        char c=st.top();
+        st.pop();
+        ns=ns+c;
+    }
+    cout<<ns<<endl;
+
+}
+int main()
+{
+    int t;
+    
+    cin>>t;
+    while(t--)
+    {
+        string s;
+        cin>>s;
+        intopost(s);
+
+    }
+}
